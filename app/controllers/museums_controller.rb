69889dc0db0700d83df1f1f7c306f382a99a99d5
Hiroki.K <po.kitagawa61@gmail.com>
2018-03-09 10:44:44 +0000

    User login out edit header fin

---
+++ app/controllers/museums_controller.rb
@@ -0,0 +1,7 @@
+class MuseumsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+	devise_for :users, controllers: {
+        sessions:      'users/sessions',
+        passwords:     'users/passwords',
+        registrations: 'users/registrations'
+    }
+
+    resources :museums
+    resources :users
+
+    root 'museums#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
